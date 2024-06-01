3f61c995b700f7581737a6c61aaec2491fe5a91d
PablCendrero8310 <>
2024-06-01 18:05:18 +0200

    Config Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  site: 'https://pablcendrero8310.github.io/',
+  base: 'history-proyect'
 });
